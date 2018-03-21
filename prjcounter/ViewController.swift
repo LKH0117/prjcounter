285db1f168594e4e72b4523dca65dcad4a6b47cb
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-03-21 12:23:41 +0900

    count color

--- prjcounter/ViewController.swift
+++ prjcounter/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var count: UILabel!
     var V = 0;
+    var co = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,6 +28,16 @@ class ViewController: UIViewController {
     @IBAction func btpressC(_ sender: Any) {
         V = 0
         count.text = ("\(V)")
+    }
+    @IBAction func btcolor(_ sender: Any) {
+        if co == 0 {
+            self.view.backgroundColor = UIColor.yellow
+            co = co + 1
+        }
+        else if co == 1 {
+            self.view.backgroundColor = UIColor.red
+            co = co - 1
+        }
     }
     
     override func didReceiveMemoryWarning() {
