92e638ea98a9f2a878d2d1bf5925fc594aaa1428
D7702_09 <d7702_09@d7702-09ui-imac.local>
2018-03-21 12:21:04 +0900

    count clean

--- prjcounter/ViewController.swift
+++ prjcounter/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var count: UILabel!
+    var V = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    @IBAction func btpress(_ sender: Any) {
+        V = V + 1
+        count.text = ("\(V)")
+    }
+    
+    @IBAction func btpressC(_ sender: Any) {
+        V = 0
+        count.text = ("\(V)")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
